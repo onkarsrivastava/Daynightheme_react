16b0e3724c352cc1214fb8b79a34497816a921a3
Onkar Srivastava <onkarsrivastava@Onkars-MacBook-Pro.local>
2024-09-22 14:39:19 +0530

    added logic

---
+++ src/components/DayNight/index.jsx
@@ -0,0 +1,26 @@
+
+import useLocalStorage from "./useLocalStorage";
+
+
+
+
+
+export default function LightDarkMode(){
+
+
+      const[theme,setTheme] = useLocalStorage('theme',"dark")
+
+      function handleToggleTheme(){
+        setTheme(theme === 'light' ?  'dark' : 'light')
+      }
+      console.log(theme);
+    return (
+    <div className="light-dark-mode">
+        <div className="container">
+            <p>Hello World!</p>
+            <button onClick={handleToggleTheme}>Change Theme</button>
+           
+        </div>
+    </div>
+    );
+}
